e1d6d36942e0518097cb657ffccf0334057a0edb
letyui <leticiayuilymo@gmail.com>
2025-10-02 15:05:26 -0300

    atualização 2/10/25
    Tentei arrumar e deu mais errado porém preciso salvar qualquer progresso

--- WebApp/cadastro.php
+++ WebApp/cadastro.php
@@ -47,6 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
@@ -79,12 +80,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <option value="A">Administrador</option>
                 </select>
             </div>
-            <button type="submit">Cadastrar</button>
+            <div>
+                <button type="submit">Cadastrar</button>
+            </div>
     </div>
     </form>
-    <p>Já tem conta? Ir para o login</p>
+    <a href="login.php">
+        <p>Já tem conta? Ir para o login</p>
+    </a>
     </div>
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 </html>
